dcc0d43206268a0a8cf98b4e9284ddd489062d3c
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2021-11-24 10:12:13 +0700

    make homepage without logic

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/home/TrandingItem.jsx
@@ -0,0 +1,64 @@
+// library
+import React from "react";
+import { Link } from "react-router-dom";
+// css
+import styles from "./TrandingItem.module.css";
+// assets
+import Blog1 from "../../assets/images/popular-post/m-blog-1.jpg";
+
+export default function TrandingItem() {
+  return (
+    <div className={`${styles.trandingContent}`}>
+      <div className={`${styles.posts}`}>
+        <div className={`${styles.postContent} mb-5`}>
+          <div className={`${styles.postImage}`}>
+            <div>
+              <img
+                src={Blog1}
+                alt="trandingBlog1"
+                className={`${styles.img}`}
+              />
+            </div>
+            <div className={`${styles.postInfo}`}>
+              <span>
+                <i className="fas fa-user" style={{ color: "#C4C4C4" }}>
+                  &nbsp;&nbsp;User 1
+                </i>
+              </span>
+              <span>
+                <i className="fas fa-calendar-alt" style={{ color: "#C4C4C4" }}>
+                  &nbsp;&nbsp;November 14, 2021
+                </i>
+              </span>
+              <span>
+                <i className="fas fa-comment " style={{ color: "#C4C4C4" }}>
+                  &nbsp;&nbsp;2 Coments
+                </i>
+              </span>
+            </div>
+          </div>
+          <div className={`${styles.postTitle} `}>
+            <Link to="" style={{ textDecoration: "none", color: "black" }}>
+              Rinjani
+            </Link>
+            <p style={{ color: "#686666da" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+              voluptas deserunt beatae adipisci iusto totam placeat corrupti
+              ipsum, tempora magnam incidunt aperiam tenetur a nobis, voluptate,
+              numquam architecto fugit. Eligendi quidem ipsam ducimus minus
+              magni illum similique veniam tempore unde?
+            </p>
+            <button
+              className={`${styles.postBtn} btn text-white px-4 mt-2 `}
+              style={{ backgroundColor: "#3086AC" }}
+              type="submit"
+            >
+              Read More
+            </button>
+          </div>
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
+}
