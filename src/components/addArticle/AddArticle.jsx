827692b7ae9c4d3c44e400ff523313d4433bc57c
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2021-11-29 03:08:49 +0700

    fix addArticlePage

--- src/components/addArticle/AddArticle.jsx
+++ src/components/addArticle/AddArticle.jsx
@@ -7,9 +7,9 @@ export default function AddArticle() {
     <div className={`${styles.newArticle} mb-5`}>
       <h3 className="text-center mt-5">Add New Article</h3>
       <div className=" mt-5">
-        <div class="form-floating">
+        <div className="form-floating">
           <textarea
-            class="form-control"
+            className="form-control"
             placeholder="Add Title"
             id="floatingTextarea"
           ></textarea>
@@ -18,7 +18,7 @@ export default function AddArticle() {
         <div className="mt-3 mb-5">
           <div class="form-floating">
             <textarea
-              class="form-control"
+              className="form-control"
               placeholder="Add Description"
               style={{ height: "400px" }}
               id="floatingTextarea"
@@ -26,10 +26,13 @@ export default function AddArticle() {
             <label for="floatingTextarea">Add Description</label>
           </div>
         </div>
+        <div className="mt-3 mb-5">
+          <input type="file" />
+        </div>
       </div>
       <div class="d-grid gap-2">
         <button
-          className={`btn text-white px-4 mt-2 d-flex justify-content-center`}
+          className={`btn text-white px-4 d-flex justify-content-center`}
           style={{ backgroundColor: "#5B5B5B" }}
         >
           Submit Post
