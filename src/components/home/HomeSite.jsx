c02a2529eb87a7d380d2b32039bc6ca9a4dd36a0
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2021-12-02 02:07:28 +0700

    fix responsive homepage

--- src/components/home/HomeSite.jsx
+++ src/components/home/HomeSite.jsx
@@ -13,8 +13,10 @@ export default function HomeSite() {
       <div className="container">
         <div className="row">
           <div className="col-7 mx-auto text-center">
-            <p className="text-white fw-light">Let's Hike in</p>
-            <h1 className="text-white fs-2">Amazing Mounts of Indonesia</h1>
+            <p className={`${styles.P} text-white fw-light `}>Let's Hike in</p>
+            <h1 className={`${styles.H1} text-white fs-2 `}>
+              Amazing Mounts of Indonesia
+            </h1>
             <Link
               className={`${styles.exploreBtn} btn text-white px-4 mt-2 rounded-pill`}
               style={{ backgroundColor: "#3086AC" }}
